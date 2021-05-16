5baa809404520662ec5dbf3a489abd4c8d241ff2
FedirM <tedmoster@gmail.com>
2021-05-16 14:27:44 +0300

    Tables

--- ui/main-window/db/main.js
+++ ui/main-window/db/main.js
@@ -13,9 +13,32 @@ function init() {
     for(let el of document.getElementsByClassName('nav-link')){
         el.addEventListener('click', navTabClick);
     }
+
+    initProffesorsTable();
 }
 
 function navTabClick(event) {
     state.navTab = event.target.id;
     console.log(state.navTab);
+}
+
+function initProffesorsTable() {
+    const tbody = document.getElementById('proffesor-table-body');
+
+    for(let i = 2; i < 30; i++){
+        tbody.insertAdjacentHTML('beforeend', `<tr>
+        <td>${i}</td>
+        <td>Книш Л. І.</td>
+        <td>Книш Людмила Іванівна</td>
+        <td>
+            <select class="selectpicker" data-live-search="true">
+                <option>N/A</option>
+                <option>Mustard</option>
+                <option>Ketchup</option>
+                <option>Книш Людмила Іванівна</option>
+            </select>
+        </td>
+        <td>5</td>
+    </tr>`);
+    }
 }
