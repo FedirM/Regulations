193785222849e26f1c0a8830e74c2c3e3c7d3a5d
FedirM <tedmoster@gmail.com>
2021-05-19 00:50:49 +0300

    Table

--- ui/main-window/db/main.js
+++ ui/main-window/db/main.js
@@ -56,17 +56,13 @@ function initDegreeTable() {
                     <input type="email" class="form-control" id="degree-alias-${d.d_id}" value="${d.d_alias}" />
                 </div>
                 <div class="col-1 text-center pt-1">
-                    <button id="btn-delete-degree-${d.d_id}" class="btn btn-outline-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </button>
+                    <button id="btn-delete-degree-${d.d_id}" class="btn btn-outline-danger bi bi-trash"></button>
                 </div>
             </div>
         </div>`);
         document.getElementById(`degree-name-${d.d_id}`).addEventListener('input', onDegreeNameChange);
         document.getElementById(`degree-alias-${d.d_id}`).addEventListener('input', onDegreeAliasChange);
+        document.getElementById(`btn-delete-degree-${d.d_id}`).addEventListener('click', onDegreeDelete);
     }
 }
 
@@ -82,16 +78,12 @@ function initSubjectTable() {
                     <input type="email" class="form-control" id="sub-name-${s.s_id}" value="${s.s_name}" />
                 </div>
                 <div class="col-1 text-center pt-1">
-                    <button id="btn-delete-sub-${s.s_id}" class="btn btn-outline-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </button>
+                    <button id="btn-delete-sub-${s.s_id}" class="btn btn-outline-danger bi bi-trash"></button>
                 </div>
             </div>
         </div>`);
         document.getElementById(`sub-name-${s.s_id}`).addEventListener('input', onSubNameChange);
+        document.getElementById(`btn-delete-sub-${s.s_id}`).addEventListener('click', onSubDelete);
     }
 }
 
@@ -124,18 +116,14 @@ function initProfessorsTable() {
                     <button id="btn-prof-sub-${p.p_id}" class="btn btn-outline-primary px-5">Переглянути</button>
                 </div>
                 <div class="col-1 text-center pt-1">
-                    <button id="btn-delete-prof-${p.p_id}" class="btn btn-outline-danger">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                        </svg>
-                    </button>
+                    <button id="btn-delete-prof-${p.p_id}" class="btn btn-outline-danger bi bi-trash"></button>
                 </div>
             </div>
         </div>`);
         document.getElementById(`prof-name-${p.p_id}`).addEventListener('input', onProfNameChange);
         document.getElementById(`prof-alias-${p.p_id}`).addEventListener('input', onProfAliasChange);
         document.getElementById(`prof-degree-${p.p_id}`).addEventListener('change', onProfDegreeChange);
+        document.getElementById(`btn-delete-prof-${p.p_id}`).addEventListener('click', onProfDelete);
     }
 
     $('.selectpicker').selectpicker('refresh');
@@ -179,6 +167,18 @@ function onDegreeAliasChange(event) {
     const idx = db.degree.findIndex(e => e.d_id === id);
     let tmp = JSON.parse(JSON.stringify(db));
     tmp.degree[idx].d_alias = event.target.value;
+}
+
+function onProfDelete(event) {
+    console.log('Delete id: ', event.target.id);
+}
+
+function onSubDelete(event) {
+    console.log('Delete id: ', event.target.id);
+}
+
+function onDegreeDelete(event) {
+    console.log('Delete id: ', event.target.id);
 }
 
 // IPC
